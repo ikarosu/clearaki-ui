0c32640de98cedac80a825270d21f9d8c8dc45c7
Clear Aki <clearaki@qq.com>
2019-05-08 10:32:52 +0800

    F 修复动态修改min/max样式不改变

--- packages/counter/Counter.vue
+++ packages/counter/Counter.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="aki-counter">
-    <svg :fill="minusColor" width=24 viewBox="0 0 1024 1024" @click="handleMinus">
+    <svg :fill="minusColor" width="24" viewBox="0 0 1024 1024" @click="handleMinus">
       <path d="M696 480H328c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h368c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zM512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64z m0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
     </svg>
     <span v-text="count"></span>
-    <svg :fill="plusColor" width=24 viewBox="0 0 1024 1024" @click="handlePlus">
+    <svg :fill="plusColor" width="24" viewBox="0 0 1024 1024" @click="handlePlus">
       <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64z m192 472c0 4.4-3.6 8-8 8H544v152c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V544H328c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h152V328c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v152h152c4.4 0 8 3.6 8 8v48z"></path>
     </svg>
   </div>
@@ -52,17 +52,26 @@ export default {
   watch: {
     count: {
       immediate: true,
-      handler(num) {
-        if (num === this.min) this.minusColor = this.disabledColor
-        else if (num < this.min) this.$emit('change', this.min)
-        else this.minusColor = this.fillColor
-        if (num === this.max) this.plusColor = this.disabledColor
-        else if (num > this.max) this.$emit('change', this.max)
-        else this.plusColor = this.fillColor
+      handler() {
+        this.edgeTest()
       }
+    },
+    min() {
+      this.edgeTest()
+    },
+    max() {
+      this.edgeTest()
     }
   },
   methods: {
+    edgeTest() {
+      if (this.count === this.min) this.minusColor = this.disabledColor
+      else if (this.count < this.min) this.$emit('change', this.min)
+      else this.minusColor = this.fillColor
+      if (this.count === this.max) this.plusColor = this.disabledColor
+      else if (this.count > this.max) this.$emit('change', this.max)
+      else this.plusColor = this.fillColor
+    },
     handleMinus() {
       if (typeof this.min === 'number') {
         if (this.count - this.step >= this.min)
